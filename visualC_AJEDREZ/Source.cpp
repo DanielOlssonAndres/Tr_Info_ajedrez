1fd93eda7516a9937ffb0c28857c1579deba5664
marcosrechi <marcos.rechina@alumnos.upm.es>
2024-02-06 20:19:16 +0100

    Primeras ideas del trabajo

--- visualC_AJEDREZ/Source.cpp
+++ visualC_AJEDREZ/Source.cpp
@@ -1,1 +1,36 @@
 // aqui escribimos el código
+
+
+// Cositas del trabajo del minichess
+
+// #define NUMPIEZAS 5
+// #define TAMTABLERO 4
+
+/*struct pieza {
+	char tipo;
+	char id;
+	bool vivo;
+	int posx;
+	int posy;
+};
+
+int main() {
+	pieza blancas[NUMPIEZAS];
+	pieza negras[NUMPIEZAS];
+
+	puts("Estos son los identificadores de cada pieza");
+	puts("Rey K");
+	puts("Reina Q");
+	puts("Torre T");
+	puts("Alfil A");
+	puts("Caballo C");
+	puts("Peon P");
+
+	do {
+		puts("Introduzca el movimiento deseado de la siguiente manera:");
+		puts("P2 C4 (mueve la el peón número 2 a la posición C4)");
+	} while (blancas[0].vivo == true || negras[0].vivo == true);
+
+	return 0;
+}
+*/
