dc93aa369fb80148cfefa1eaa2824f3c16b425b1
Carlotagutierrezz <159192428+Carlotagutierrezz@users.noreply.github.com>
2024-04-28 03:19:22 +0200

    Update dama.cpp

--- visualC_AJEDREZ/src/dama.cpp
+++ visualC_AJEDREZ/src/dama.cpp
@@ -2,11 +2,40 @@
 #include "freeglut.h"
 
 
-void Dama::dibuja()
+
+
+void Dama::inicializa_5x6EO(const Vector2xy& posicion, const float tipo)
+{
+    this->posicion = posicion;
+    this->tipo = tipo;
+}
+
+void Dama::inicializa_PETTY(const Vector2xy& posicion, const float tipo)
+{
+    this->posicion = posicion;
+    this->tipo = tipo;
+}
+
+
+void Dama::dibuja_blanco()
 {
     glPushMatrix();
-    glColor3f(tipo, tipo, tipo);
-    glTranslatef(posicion.x, posicion.y, 0.0f); 
-    glutWireTeapot(1.5); 
+    glTranslatef(posicion.x - 3, posicion.y - 3, 1);
+
+    sprite_blanco.setSize(5, 5);
+
+    sprite_blanco.draw();
     glPopMatrix();
 }
+
+void Dama::dibuja_negro()
+{
+    glPushMatrix();
+    glTranslatef(posicion.x - 3, posicion.y - 3, 1);
+
+    sprite_negro.setSize(5, 5);
+
+    sprite_negro.draw();
+    glPopMatrix();
+}
+
