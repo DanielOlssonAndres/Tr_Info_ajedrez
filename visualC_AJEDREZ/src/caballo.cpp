b446ed7ef0158e0f5cbe3b137a6ac603d9729c7f
Carlotagutierrezz <159192428+Carlotagutierrezz@users.noreply.github.com>
2024-04-28 03:26:31 +0200

    Update caballo.cpp

--- visualC_AJEDREZ/src/caballo.cpp
+++ visualC_AJEDREZ/src/caballo.cpp
@@ -1,6 +1,20 @@
 #include "caballo.h"
 #include "freeglut.h"
 
+
+void Caballo::inicializa_5x6EO(const Vector2xy& posicion, const float tipo)
+{
+    this->posicion = posicion;
+    this->tipo = tipo;
+}
+
+void Caballo::inicializa_PETTY(const Vector2xy& posicion, const float tipo)
+{
+    this->posicion = posicion;
+    this->tipo = tipo;
+}
+
+
 void Caballo::dibuja_blanco()
 {
     glPushMatrix();
