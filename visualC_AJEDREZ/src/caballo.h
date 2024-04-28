9a0b179965573b5499b90f8ee5a2901920c632fd
Carlotagutierrezz <159192428+Carlotagutierrezz@users.noreply.github.com>
2024-04-28 03:20:04 +0200

    Update caballo.h

--- visualC_AJEDREZ/src/caballo.h
+++ visualC_AJEDREZ/src/caballo.h
@@ -1,13 +1,20 @@
 #pragma once
 #include "Vector2xy.h"
 #include "Ficha.h"
+#include "ETSIDI.h"
 
 class Caballo : public Ficha
 { 
 
 public:
-	void dibuja();
 
+	ETSIDI::Sprite sprite_blanco{ "bin/imagenes/T_CABALLO_CEBRA.png" };
+	ETSIDI::Sprite sprite_negro{ "bin/imagenes/A_CABALLO_CABALLITO_DE_MAR.png" };
+
+	void inicializa_5x6EO(const Vector2xy& posicion, const float tipo);
+	void inicializa_PETTY(const Vector2xy& posicion, const float tipo);
+	void dibuja_blanco();
+	void dibuja_negro();
 
 };
 
