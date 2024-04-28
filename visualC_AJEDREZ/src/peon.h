6533c5f92f0716d5f2e50e93ed479ba880efd5d1
Carlotagutierrezz <159192428+Carlotagutierrezz@users.noreply.github.com>
2024-04-28 03:08:11 +0200

    Update peon.h

--- visualC_AJEDREZ/src/peon.h
+++ visualC_AJEDREZ/src/peon.h
@@ -6,6 +6,12 @@ class Peon : public Ficha
 {
 
 public:
-	void dibuja();
+	ETSIDI::Sprite sprite_blanco{ "bin/imagenes/T_PEON_CONEJO_2.png"};
+	ETSIDI::Sprite sprite_negro{ "bin/imagenes/A_PEON_GAMBA.png" };
+
+	void inicializa(const Vector2xy& posicion, const float tipo);
+
+	void dibuja_blanco();
+	void dibuja_negro();
 };
 
