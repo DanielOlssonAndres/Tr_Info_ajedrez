e9aa474d08a32145ee026649503ddb98b79f1f98
Carlotagutierrezz <159192428+Carlotagutierrezz@users.noreply.github.com>
2024-04-28 11:18:39 +0200

    Cambios para ver png V1

--- visualC_AJEDREZ/src/alfil.h
+++ visualC_AJEDREZ/src/alfil.h
@@ -6,8 +6,8 @@ class Alfil : public Ficha
 {
 
 public:
-	ETSIDI::Sprite sprite_blanco{ "bin/imagenes/T_ALFIL_PATO.png" };
-	ETSIDI::Sprite sprite_negro{ "bin/imagenes/A_ALFIL_PULPO.png" };
+	ETSIDI::Sprite sprite_blanco{ "imagenes/T_ALFIL_PATO.png" };
+	ETSIDI::Sprite sprite_negro{ "imagenes/A_ALFIL_PULPO.png" };
 	
 	void dibuja_blanco();
 	void dibuja_negro();
