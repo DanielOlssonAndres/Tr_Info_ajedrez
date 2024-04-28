d31e21dfd65689419fba6b0d605844389a6bd1df
Carlotagutierrezz <159192428+Carlotagutierrezz@users.noreply.github.com>
2024-04-28 11:16:31 +0200

    Cambios para ver png V1

--- visualC_AJEDREZ/src/torre.h
+++ visualC_AJEDREZ/src/torre.h
@@ -8,8 +8,8 @@ class Torre : public Ficha
 
 
 public:
-	ETSIDI::Sprite sprite_blanco{ "bin/imagenes/T_TORRE_GIRAFA.png" };
-	ETSIDI::Sprite sprite_negro{ "bin/imagenes/A_TORRES_CANGREJO.png" };
+	ETSIDI::Sprite sprite_blanco{ "imagenes/T_TORRE_GIRAFA.png" };
+	ETSIDI::Sprite sprite_negro{ "imagenes/A_TORRES_CANGREJO.png" };
 
 	void dibuja_blanco();
 	void dibuja_negro();
