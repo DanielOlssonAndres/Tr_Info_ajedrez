c9039a4a1e1e770cb4061df09697cfc6326b420b
Carlotagutierrezz <159192428+Carlotagutierrezz@users.noreply.github.com>
2024-04-28 03:07:13 +0200

    Update rey.h

--- visualC_AJEDREZ/src/rey.h
+++ visualC_AJEDREZ/src/rey.h
@@ -1,13 +1,19 @@
 #pragma once
 #include "Vector2xy.h"
 #include "Ficha.h"
-
+#include "ETSIDI.h"
 class Rey : public Ficha
 {
 
 
 public:
 	
-	void dibuja();
+	ETSIDI::Sprite sprite_blanco{ "bin/imagenes/T_REY_LEON.png" };
+	ETSIDI::Sprite sprite_negro{ "bin/imagenes/A_REY_TIBURON.png" };
+
+	void inicializa_5x6EO(const Vector2xy& posicion, const float tipo);
+	void inicializa_PETTY(const Vector2xy& posicion, const float tipo);
+	void dibuja_blanco();
+	void dibuja_negro();
 };
 
