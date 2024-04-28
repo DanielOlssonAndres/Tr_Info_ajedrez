f5754cf708492570fa12917d280b0147ff033b72
Carlotagutierrezz <159192428+Carlotagutierrezz@users.noreply.github.com>
2024-04-28 11:17:43 +0200

    Cambios para ver png V1

--- visualC_AJEDREZ/src/dama.h
+++ visualC_AJEDREZ/src/dama.h
@@ -8,8 +8,8 @@ class Dama : public Ficha
 
 public:
 
-	ETSIDI::Sprite sprite_blanco{ "bin/imagenes/T_DAMA_LEONA.png" };
-	ETSIDI::Sprite sprite_negro{ "bin/imagenes/A_DAMA_BALLENA.png" };
+	ETSIDI::Sprite sprite_blanco{ "imagenes/T_DAMA_LEONA.png" };
+	ETSIDI::Sprite sprite_negro{ "imagenes/A_DAMA_BALLENA.png" };
 
 	void dibuja_blanco();
 	void dibuja_negro();
