cf7b621c10b8d4ba7bec509d5096c07b690c5137
Carlotagutierrezz <159192428+Carlotagutierrezz@users.noreply.github.com>
2024-04-28 03:05:18 +0200

    Nuevas torres

--- visualC_AJEDREZ/src/torre.h
+++ visualC_AJEDREZ/src/torre.h
@@ -8,6 +8,12 @@ class Torre : public Ficha
 
 
 public:
-	void dibuja();
+	ETSIDI::Sprite sprite_blanco{ "bin/imagenes/T_TORRE_GIRAFA.png" };
+	ETSIDI::Sprite sprite_negro{ "bin/imagenes/A_TORRES_CANGREJO.png" };
+
+	void inicializa_5x6EO(const Vector2xy& posicion, const float tipo);
+	void inicializa_PETTY(const Vector2xy& posicion, const float tipo);
+	void dibuja_blanco();
+	void dibuja_negro();
 };
 
