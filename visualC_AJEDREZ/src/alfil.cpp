f9afdb34fbdb008a4868c490970cb3a466527593
Carlotagutierrezz <159192428+Carlotagutierrezz@users.noreply.github.com>
2024-04-28 03:21:34 +0200

    Update alfil.cpp

--- visualC_AJEDREZ/src/alfil.cpp
+++ visualC_AJEDREZ/src/alfil.cpp
@@ -1,11 +1,39 @@
 #include "alfil.h"
 #include "freeglut.h"
 
-void Alfil::dibuja()
+
+
+void Alfil::inicializa_5x6EO(const Vector2xy& posicion, const float tipo)
+{
+    this->posicion = posicion;
+    this->tipo = tipo;
+}
+
+void Alfil::inicializa_PETTY(const Vector2xy& posicion, const float tipo)
+{
+    this->posicion = posicion;
+    this->tipo = tipo;
+}
+
+
+void Alfil::dibuja_blanco()
 {
     glPushMatrix();
-    glColor3f(tipo, tipo, tipo);
-    glTranslatef(posicion.x, posicion.y, 0.0f); 
-    glutWireSphere(2, 30, 30); 
+    glTranslatef(posicion.x - 3, posicion.y - 3, 1);
+
+    sprite_blanco.setSize(5, 5);
+
+    sprite_blanco.draw();
+    glPopMatrix();
+}
+
+void Alfil::dibuja_negro()
+{
+    glPushMatrix();
+    glTranslatef(posicion.x - 3, posicion.y - 3, 1);
+
+    sprite_negro.setSize(5, 5);
+
+    sprite_negro.draw();
     glPopMatrix();
 }
