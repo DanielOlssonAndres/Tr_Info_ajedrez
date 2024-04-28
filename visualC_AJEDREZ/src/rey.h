5dc565599e538df988dcdc0736858e428444ecd6
Carlotagutierrezz <159192428+Carlotagutierrezz@users.noreply.github.com>
2024-04-28 11:16:59 +0200

    Cambios para ver png V1

--- visualC_AJEDREZ/src/rey.h
+++ visualC_AJEDREZ/src/rey.h
@@ -8,8 +8,8 @@ class Rey : public Ficha
 
 public:
 	
-	ETSIDI::Sprite sprite_blanco{ "bin/imagenes/T_REY_LEON.png" };
-	ETSIDI::Sprite sprite_negro{ "bin/imagenes/A_REY_TIBURON.png" };
+	ETSIDI::Sprite sprite_blanco{ "imagenes/T_REY_LEON.png" };
+	ETSIDI::Sprite sprite_negro{ "imagenes/A_REY_TIBURON.png" };
 
 	void dibuja_blanco();
 	void dibuja_negro();
