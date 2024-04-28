791ccacea20a488ee1cee461a5e26317a8d3ee1c
Carlotagutierrezz <159192428+Carlotagutierrezz@users.noreply.github.com>
2024-04-28 11:18:18 +0200

    Cambios para ver png V1

--- visualC_AJEDREZ/src/caballo.h
+++ visualC_AJEDREZ/src/caballo.h
@@ -7,8 +7,8 @@ class Caballo : public Ficha
 
 public:
 
-	ETSIDI::Sprite sprite_blanco{ "bin/imagenes/T_CABALLO_CEBRA.png" };
-	ETSIDI::Sprite sprite_negro{ "bin/imagenes/A_CABALLO_CABALLITO_DE_MAR.png" };
+	ETSIDI::Sprite sprite_blanco{ "imagenes/T_CABALLO_CEBRA.png" };
+	ETSIDI::Sprite sprite_negro{ "imagenes/A_CABALLO_CABALLITO_DE_MAR.png" };
 
 	void dibuja_blanco();
 	void dibuja_negro();
