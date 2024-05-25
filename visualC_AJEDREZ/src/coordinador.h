57d43d5407de669f4ca34e4caf78b6710fdf1aaf
Carlotagutierrezz <159192428+Carlotagutierrezz@users.noreply.github.com>
2024-05-25 12:56:18 +0200

    Update coordinador.h

---
+++ visualC_AJEDREZ/src/coordinador.h
@@ -0,0 +1,16 @@
+#pragma once
+#include "mundo.h"
+#include "Tablero.h"
+#include "ETSIDI.h"
+
+class coordinador
+{
+	enum estado{juego,inicio,vs1_menu,vsm_menu,start,vs1_5x6_menu,vs1_5x6_instr, vs1_petty_menu, vs1_petty_instr} estado{};
+	
+	Mundo mundo;
+
+public:
+	void tecla(unsigned char key);
+	void dibuja();
+};
+
