f3424d0a1c056e51f0776a1589dfd15c5fa3c785
Carlotagutierrezz <159192428+Carlotagutierrezz@users.noreply.github.com>
2024-05-25 13:37:19 +0200

    raton coord

--- visualC_AJEDREZ/src/coordinador.h
+++ visualC_AJEDREZ/src/coordinador.h
@@ -5,12 +5,15 @@
 
 class coordinador
 {
-	enum estado{juego,inicio,vs1_menu,vsm_menu,start,vs1_5x6_menu,vs1_5x6_instr, vs1_petty_menu, vs1_petty_instr} estado{};
-	
+	enum estado { juego, inicio, vs1_menu, vsm_menu, start, vs1_5x6_menu, vs1_5x6_instr, vs1_petty_menu, vs1_petty_instr } estado{};
+
 	Mundo mundo;
 
 public:
 	void tecla(unsigned char key);
 	void dibuja();
+	void MouseButton(int x, int y, int boton, bool abajo, bool TeclaSp, bool TeclaCtr);
+
+
 };
 
