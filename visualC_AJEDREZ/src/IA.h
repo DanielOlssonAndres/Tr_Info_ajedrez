ca254b94cb67598756adea17bc0e492b1e0d5ea6
lunasbaciero <luna.silveira@alumnos.upm.es>
2024-05-27 11:23:25 +0200

    IA+MENU_VSMAQ

---
+++ visualC_AJEDREZ/src/IA.h
@@ -0,0 +1,24 @@
+#pragma once
+/*
+#include "freeglut.h"
+#include "coordinador.h"
+
+
+class IA{
+
+protected :
+	bool color;
+public:
+	IA(bool col = true) {color = col; }
+	int E = coordinador.Get_Estado();
+	bool movimiento(int E);
+
+};
+
+class IA_aleatorio :public IA {
+public:
+	IA_aleatorio(bool col = true) { color = col; }
+	int E = coordinador.Get_Estado();
+	bool movimiento(int E);
+
+};*/
--- visualC_AJEDREZ/src/coordinador.cpp
+++ visualC_AJEDREZ/src/coordinador.cpp
@@ -124,6 +124,88 @@ void coordinador::dibuja() {
 		glEnable(GL_LIGHTING);
 		glDisable(GL_TEXTURE_2D);
 		break;
+
+	case VSM_MENU:
+
+		glEnable(GL_TEXTURE_2D);
+		glBindTexture(GL_TEXTURE_2D,
+			ETSIDI::getTexture("imagenes/8_MACHINE_MENU.png").id);
+		glDisable(GL_LIGHTING);
+		glBegin(GL_POLYGON);
+		glColor3f(1, 1, 1);
+		glTexCoord2d(0, 1); glVertex2d(-15, -4); //inferior izquierda
+		glTexCoord2d(1, 1); glVertex2d(45, -4); //Inferior derecha
+		glTexCoord2d(1, 0); glVertex2d(45, 40); //Superior derecha
+		glTexCoord2d(0, 0); glVertex2d(-15, 40); //Superior izquierda
+		glEnd();
+		glEnable(GL_LIGHTING);
+		glDisable(GL_TEXTURE_2D);
+		break;
+	case VSM_5x6_MENU:
+
+		glEnable(GL_TEXTURE_2D);
+		glBindTexture(GL_TEXTURE_2D,
+			ETSIDI::getTexture("imagenes/9_MACHINE_5X6_MENU.png").id);
+		glDisable(GL_LIGHTING);
+		glBegin(GL_POLYGON);
+		glColor3f(1, 1, 1);
+		glTexCoord2d(0, 1); glVertex2d(-15, -4); //inferior izquierda
+		glTexCoord2d(1, 1); glVertex2d(45, -4); //Inferior derecha
+		glTexCoord2d(1, 0); glVertex2d(45, 40); //Superior derecha
+		glTexCoord2d(0, 0); glVertex2d(-15, 40); //Superior izquierda
+		glEnd();
+		glEnable(GL_LIGHTING);
+		glDisable(GL_TEXTURE_2D);
+		break;
+	case VSM_5x6_INSTR:
+
+		glEnable(GL_TEXTURE_2D);
+		glBindTexture(GL_TEXTURE_2D,
+			ETSIDI::getTexture("imagenes/10_MACHINE_5X6_INSTRUCTIONS.png").id);
+		glDisable(GL_LIGHTING);
+		glBegin(GL_POLYGON);
+		glColor3f(1, 1, 1);
+		glTexCoord2d(0, 1); glVertex2d(-15, -4); //inferior izquierda
+		glTexCoord2d(1, 1); glVertex2d(45, -4); //Inferior derecha
+		glTexCoord2d(1, 0); glVertex2d(45, 40); //Superior derecha
+		glTexCoord2d(0, 0); glVertex2d(-15, 40); //Superior izquierda
+		glEnd();
+		glEnable(GL_LIGHTING);
+		glDisable(GL_TEXTURE_2D);
+		break;
+	case VSM_PETTY_MENU:
+
+		glEnable(GL_TEXTURE_2D);
+		glBindTexture(GL_TEXTURE_2D,
+			ETSIDI::getTexture("imagenes/11_MACHINE_PETTY_MENU.png").id);
+		glDisable(GL_LIGHTING);
+		glBegin(GL_POLYGON);
+		glColor3f(1, 1, 1);
+		glTexCoord2d(0, 1); glVertex2d(-15, -4); //inferior izquierda
+		glTexCoord2d(1, 1); glVertex2d(45, -4); //Inferior derecha
+		glTexCoord2d(1, 0); glVertex2d(45, 40); //Superior derecha
+		glTexCoord2d(0, 0); glVertex2d(-15, 40); //Superior izquierda
+		glEnd();
+		glEnable(GL_LIGHTING);
+		glDisable(GL_TEXTURE_2D);
+		break;
+	case VSM_PETTY_INSTR:
+
+		glEnable(GL_TEXTURE_2D);
+		glBindTexture(GL_TEXTURE_2D,
+			ETSIDI::getTexture("imagenes/12_MACHINE_PETTY_INSTRUCTIONS.png").id);
+		glDisable(GL_LIGHTING);
+		glBegin(GL_POLYGON);
+		glColor3f(1, 1, 1);
+		glTexCoord2d(0, 1); glVertex2d(-15, -4); //inferior izquierda
+		glTexCoord2d(1, 1); glVertex2d(45, -4); //Inferior derecha
+		glTexCoord2d(1, 0); glVertex2d(45, 40); //Superior derecha
+		glTexCoord2d(0, 0); glVertex2d(-15, 40); //Superior izquierda
+		glEnd();
+		glEnable(GL_LIGHTING);
+		glDisable(GL_TEXTURE_2D);
+		break;
+
 	case JUEGO:
 		// mundo.inicializa();
 		mundo.dibuja();
@@ -148,8 +230,10 @@ void coordinador::tecla(unsigned char key) {
 			estado = VS1_MENU;
 			ETSIDI::play("sonidos/InicioJuego.wav");
 		}
-		
-		//if (key == 'm') { estado = ; }
+		if (key == 'm' || key == 'M') {//VSMAQ
+			estado = VSM_MENU;
+			ETSIDI::play("sonidos/InicioJuego.wav");
+		}
 		break;
 		
 	case VS1_MENU:
@@ -202,9 +286,60 @@ void coordinador::tecla(unsigned char key) {
 			estado = VS1_PETTY_MENU;
 		}
 		break;
-	case JUEGO://de momento no hay teclas durante el juego.
-		
 
+	case VSM_MENU:
+		if (key == 'a' || key == 'A') {
+			estado = VSM_5x6_MENU;
+			ETSIDI::play("sonidos/InicioJuego.wav");
+		}
+		else if (key == 'b' || key == 'B') {
+			estado = VSM_PETTY_MENU;
+			ETSIDI::play("sonidos/InicioJuego.wav");
+		}
+
+		break;
+
+	case VSM_5x6_MENU:
+		if (key == 'i' || key == 'I') {
+			estado = VSM_5x6_INSTR;
+			ETSIDI::play("sonidos/InicioJuego.wav");
+		}
+		else if (key == 'p' || key == 'P') {
+			//tablero.inicializa(0);//Tablero 5x6
+			mundo.inicializa(0);
+			estado = JUEGO;
+			ETSIDI::play("sonidos/InicioJuego.wav");
+		}
+
+		break;
+	case VSM_5x6_INSTR:
+		if (key == 'y' || key == 'Y') {
+			ETSIDI::play("sonidos/InicioJuego.wav");
+			estado = VSM_5x6_MENU;
+		}
+		break;
+	case VSM_PETTY_MENU:
+		if (key == 'i' || key == 'I') {
+			estado = VSM_PETTY_INSTR;
+			ETSIDI::play("sonidos/InicioJuego.wav");
+		}
+		else if (key == 'p' || key == 'P') {
+			//tablero.inicializa(1);//Tablero petty
+			mundo.inicializa(1);
+			estado = JUEGO;
+			ETSIDI::play("sonidos/InicioJuego.wav");
+		}
+		break;
+
+	case VSM_PETTY_INSTR:
+		if (key == 'y' || key == 'Y') {
+			ETSIDI::play("sonidos/InicioJuego.wav");
+			estado = VSM_PETTY_MENU;
+		}
+		break;
+
+
+	case JUEGO://de momento no hay teclas durante el juego.
 		break;
 		
 	default:
